50a672f1bffb6b698e0039157e7fa130bf6361b7
jagannath20-dev <devarakondajagannath2580@gmail.com>
2022-12-12 17:58:59 +0530

    updated

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import pymongo
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys,os
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-# Database Name
-dataBase = client["neurolabDB"]
+def test_logger_and_exception():
+     try:
+          logging.info("Starting the test_logger_and_exception")
+          result = 3/0
+          print(result)
+          logging.info("Stopping the test_logger_and_exception")
+     except Exception as e:
+          logging.debug("Stopping the test_logger_and_exception")
 
-# Collection  Name
-collection = dataBase['Products']
+          raise SensorException(e, sys)
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
 
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
+if __name__ == "__main__":
+     try:
+          test_logger_and_exception()
+     
 
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+     except Exception as e:
+          print(e)
--- sensor/logger.py
+++ sensor/logger.py
@@ -1,10 +1,10 @@
 import logging
 import os
 from datetime import datetime
-import os 
+import os
 
 #log file name
-LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y_%H%M%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
 #log directory
 LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
@@ -12,7 +12,7 @@ LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 #create folder if not available
 os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
-#Log file path
+#log file path
 
 LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
