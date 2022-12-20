5cefc6d961c0598ac8a7c41b38b5d1cc852e8da3
jagannath20-dev <devarakondajagannath2580@yahoo.com>
2022-12-20 21:37:10 +0530

    update

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -15,7 +15,7 @@ class DataValidationArtifact:
 class DataTransformationArtifact:
     transform_object_path:str
     transformed_train_path:str
-    transformed_test:str
+    transformed_test_path:str
     target_encoder_path:str
 
 @dataclass
@@ -23,12 +23,3 @@ class ModelTrainerArtifact:
     model_path:str 
     f1_train_score:float 
     f1_test_score: float
-
-@dataclass
-class ModelEvaluationArtifact:
-    is_model_accepted:bool
-    improved_accuracy:float 
-
-class ModelPusherArtifact:
-    pusher_model_dir:str
-    saved_model_dir:str 
